82a93a3bb1273e502638ad8823c27550460a9781
Jean Piert Palomino Quispe <dragon3706@hotmail.com>
2023-04-12 18:13:01 -0500

    page no responsive

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-const vowels = ['a','e','i','o','u'];
-const replaceWord =['ai','enter','imes','ober','ufat'];
+
+/*
 function encrypt(str){
     let newWord ="";
-    let indexVowel =0;
+    let indexVowel =0; "asdasd"
     for (let index = 0; index < str.length; index++) {
         currentVowel = str.charAt(index);
         indexVowel = vowels.indexOf(currentVowel);
@@ -15,8 +15,49 @@ function encrypt(str){
         }       
     }
     return newWord; 
-
 }
-function decrypt(str){
-    
+
+*/
+/*
+funcion de desencriptacion
+*/
+
+const vowels2 = ['a','e','i','o','u'];
+const replaceWord2 =['ai','enter','imes','ober','ufat'];
+
+function decrypt3(desafio){
+
+   //let desafio = "fenterlimescimesdaidenters poberr enternfrenterntair enterstenter dentersaifimesober y haibenterrlober cobernclufatimesdober cobern enterximestober!"
+    let arrayWords = desafio.split(" ");
+    let currentWord ="";
+    let newWord="";
+    let indexVowel =0;
+    for (let index = 0; index < arrayWords.length; index++) {
+        currentWord = arrayWords[index];
+        
+        for (let j = 0; j < currentWord.length; j++) {
+            currentVowel = currentWord.charAt(j);
+            indexVowel = vowels2.indexOf(currentVowel);
+
+            if (indexVowel != -1 ){
+                console.log("slice--- " + currentWord.slice(j, j + replaceWord2[indexVowel].length));
+                console.log("replace-- "+ replaceWord2[indexVowel]);
+                if (currentWord.slice(j, j + replaceWord2[indexVowel].length) == replaceWord2[indexVowel]){
+                    newWord += vowels2[indexVowel];
+                    j += replaceWord2[indexVowel].length - 1;
+                } 
+                else {
+                    newWord += currentVowel;
+                }
+            }
+            else{
+                newWord += currentVowel;
+            }       
+            
+        }
+        newWord += " ";
+    }
+    return newWord;
+   
+   
 }
